3b11bcf4324dd46225f0779d1eb695de21b39ea2
Sophia <sophkiani@gmail.com>
2021-11-23 12:19:16 -0600

    updated the overall client structure

---
+++ client/src/components/card-home/cardHome.js
@@ -0,0 +1,9 @@
+import './card-home.css'
+
+export default function CardHome() {
+  return (
+    <div>
+      card home
+    </div>
+  )
+}
---
+++ client/src/components/card-profile/cardProfile.js
@@ -0,0 +1,9 @@
+import './card-profile.css'
+
+export default function Hero() {
+  return (
+    <div>
+      card profile
+    </div>
+  )
+}
