c5e53125687dddd8daccfadaa557c042a3018328
Laura Hofer <ljhofer@gmail.com>
2021-11-23 13:14:55 -0600

    Added pseudocoding for UserController.js

---
+++ server/controllers/UserController.js
@@ -0,0 +1,69 @@
+const { User } = require('../models');
+
+module.exports = {
+
+    // createUser
+
+
+
+    // updateUsername
+
+
+
+    // updateName 
+
+
+
+    // updateEmail
+
+
+
+    // getUser
+
+
+ 
+    // addFollowing
+        // add to other person's followers
+
+
+    // deleteFollowing
+
+
+
+    // getAllFollowing
+
+    
+
+    // getAllFollowing
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
